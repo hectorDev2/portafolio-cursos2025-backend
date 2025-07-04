2e7c5a00ff48250bd54604b90efb9c2e44e20527
Hector Barazorda <ph.barazorda@gmail.com>
2025-07-04 18:56:09 -0500

    feat: remove SilaboModule import from AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,6 @@ import { AuthModule } from './auth/auth.module';
 import { APP_GUARD } from '@nestjs/core';
 import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 import { PortfolioModule } from './portfolio/portfolio.module';
-import { SilaboModule } from './silabo/silabo.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 
@@ -15,7 +14,6 @@ import { join } from 'path';
     UserModule,
     AuthModule,
     PortfolioModule,
-    SilaboModule,
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'uploads'),
       serveRoot: '/uploads',
