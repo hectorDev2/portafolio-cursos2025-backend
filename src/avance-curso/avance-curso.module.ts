3cdb0c1b2129a01c79b0eb30b6901ded3af99558
Hector Barazorda <ph.barazorda@gmail.com>
2025-07-06 11:29:30 -0500

    feat: implement AvanceCurso module, controller, and service for course progress management

---
+++ src/avance-curso/avance-curso.module.ts
@@ -0,0 +1,12 @@
+
+import { Module } from '@nestjs/common';
+import { AvanceCursoService } from './avance-curso.service';
+import { AvanceCursoController } from './avance-curso.controller';
+import { PrismaModule } from '../prisma/prisma.module';
+
+@Module({
+  imports: [PrismaModule],
+  controllers: [AvanceCursoController],
+  providers: [AvanceCursoService],
+})
+export class AvanceCursoModule {}
