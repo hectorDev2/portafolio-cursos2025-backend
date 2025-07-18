e35a55b79df76df847868fc0cf9e72cfcfb99fd5
Hector Barazorda <ph.barazorda@gmail.com>
2025-07-18 13:25:13 -0500

    feat: agregar tablas y relaciones para el modelo de datos de la aplicación

---
+++ prisma/migrations/20250718162541_init/migration.sql
@@ -0,0 +1,114 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "email" TEXT NOT NULL,
+    "phoneNumber" TEXT DEFAULT '',
+    "address" TEXT,
+    "dateOfBirth" DATETIME,
+    "biography" TEXT,
+    "password" TEXT NOT NULL,
+    "name" TEXT,
+    "lastName" TEXT,
+    "role" TEXT NOT NULL DEFAULT 'DOCENTE',
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Portfolio" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "semester" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "teacherId" TEXT NOT NULL,
+    CONSTRAINT "Portfolio_teacherId_fkey" FOREIGN KEY ("teacherId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Curso" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "code" TEXT,
+    "portfolioId" TEXT NOT NULL,
+    CONSTRAINT "Curso_portfolioId_fkey" FOREIGN KEY ("portfolioId") REFERENCES "Portfolio" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Caratula" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "fileUrl" TEXT NOT NULL,
+    "uploadedAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "portfolioId" TEXT NOT NULL,
+    CONSTRAINT "Caratula_portfolioId_fkey" FOREIGN KEY ("portfolioId") REFERENCES "Portfolio" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "CargaLectiva" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "fileUrl" TEXT NOT NULL,
+    "uploadedAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "portfolioId" TEXT NOT NULL,
+    CONSTRAINT "CargaLectiva_portfolioId_fkey" FOREIGN KEY ("portfolioId") REFERENCES "Portfolio" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Filosofía" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "fileUrl" TEXT NOT NULL,
+    "uploadedAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "portfolioId" TEXT NOT NULL,
+    CONSTRAINT "Filosofía_portfolioId_fkey" FOREIGN KEY ("portfolioId") REFERENCES "Portfolio" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Curriculum" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "fileUrl" TEXT NOT NULL,
+    "uploadedAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "portfolioId" TEXT NOT NULL,
+    CONSTRAINT "Curriculum_portfolioId_fkey" FOREIGN KEY ("portfolioId") REFERENCES "Portfolio" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "silabo" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "version" TEXT,
+    "fileUrl" TEXT NOT NULL,
+    "uploadedAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "cursoId" TEXT NOT NULL,
+    CONSTRAINT "silabo_cursoId_fkey" FOREIGN KEY ("cursoId") REFERENCES "Curso" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "AvanceCurso" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "fileUrl" TEXT NOT NULL,
+    "uploadedAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "cursoId" TEXT NOT NULL,
+    CONSTRAINT "AvanceCurso_cursoId_fkey" FOREIGN KEY ("cursoId") REFERENCES "Curso" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "RegistroEntregaSílabo" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "fileUrl" TEXT NOT NULL,
+    "uploadedAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "cursoId" TEXT NOT NULL,
+    CONSTRAINT "RegistroEntregaSílabo_cursoId_fkey" FOREIGN KEY ("cursoId") REFERENCES "Curso" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Feedback" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "comment" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "evaluatorId" TEXT NOT NULL,
+    "portfolioId" TEXT NOT NULL,
+    CONSTRAINT "Feedback_evaluatorId_fkey" FOREIGN KEY ("evaluatorId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Feedback_portfolioId_fkey" FOREIGN KEY ("portfolioId") REFERENCES "Portfolio" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
