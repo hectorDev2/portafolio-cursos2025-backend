da321ffd0fa86d8b59d66bd0f9c2242abd4acd90
Hector Barazorda <ph.barazorda@gmail.com>
2025-07-30 23:06:20 -0500

    style: mejorar formato de las propiedades en UpdateUserDto para mayor legibilidad

--- src/user/dto/UpdateUserDto.dto.ts
+++ src/user/dto/UpdateUserDto.dto.ts
@@ -10,7 +10,11 @@ import { Role } from 'src/enum/role';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateUserDto {
-  @ApiProperty({ description: 'Correo electrónico del usuario (opcional)', example: 'new_test@unsaac.edu.pe', required: false })
+  @ApiProperty({
+    description: 'Correo electrónico del usuario (opcional)',
+    example: 'new_test@unsaac.edu.pe',
+    required: false,
+  })
   @IsOptional()
   @IsEmail({}, { message: 'El correo electrónico debe ser válido.' })
   @Matches(/@unsaac\.edu\.pe$/, {
@@ -18,18 +22,31 @@ export class UpdateUserDto {
   })
   email?: string;
 
-  @ApiProperty({ description: 'Nueva contraseña del usuario (opcional)', example: 'new_password123', required: false })
+  @ApiProperty({
+    description: 'Nueva contraseña del usuario (opcional)',
+    example: 'new_password123',
+    required: false,
+  })
   @IsOptional()
   @IsString({ message: 'La contraseña debe ser una cadena de texto.' })
   @MinLength(8, { message: 'La contraseña debe tener al menos 8 caracteres.' })
   password?: string;
 
-  @ApiProperty({ description: 'Nuevo nombre del usuario (opcional)', example: 'Jane', required: false })
+  @ApiProperty({
+    description: 'Nuevo nombre del usuario (opcional)',
+    example: 'Jane',
+    required: false,
+  })
   @IsOptional()
   @IsString({ message: 'El nombre debe ser una cadena de texto.' })
   name?: string;
 
-  @ApiProperty({ description: 'Nuevo rol del usuario (opcional)', enum: Role, example: Role.DOCENTE, required: false })
+  @ApiProperty({
+    description: 'Nuevo rol del usuario (opcional)',
+    enum: Role,
+    example: Role.DOCENTE,
+    required: false,
+  })
   @IsOptional()
   @IsEnum(Role, { message: 'Rol no válido.' })
   role?: Role;
